c435c71d87a2d4f1252f1f6873f78d107e0f4c6c
John Kessenich <cepheus@frii.com>
2013-02-06 19:46:32 +0000

    Update copyrights.
    git-svn-id: https://cvs.khronos.org/svn/repos/ogl/trunk/ecosystem/public/sdk/tools/glslang@20490 e7fa87d3-cd2b-0410-9028-fcbf551c1848

--- glslang/MachineIndependent/ParseHelper.h
+++ glslang/MachineIndependent/ParseHelper.h
@@ -1,8 +1,8 @@
 //
 //Copyright (C) 2002-2005  3Dlabs Inc. Ltd.
-//All rights reserved.
+//Copyright (C) 2012-2013 LunarG, Inc.
 //
-//Copyright (C) 2012 LunarG, Inc.
+//All rights reserved.
 //
 //Redistribution and use in source and binary forms, with or without
 //modification, are permitted provided that the following conditions
