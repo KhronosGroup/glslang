5f5b205ce971547d9bec8e9a480a42dc8b4bba48
John Kessenich <cepheus@frii.com>
2015-09-13 20:59:36 -0600

    Bump up layout(location) limit from 62 to 4094, to handle bigger uniform locations.

--- glslang/Include/Types.h
+++ glslang/Include/Types.h
@@ -513,8 +513,8 @@ public:
     int layoutOffset;
     int layoutAlign;
 
-                 unsigned int layoutLocation         : 7;
-    static const unsigned int layoutLocationEnd =   0x3F;
+                 unsigned int layoutLocation         :12;
+    static const unsigned int layoutLocationEnd =  0xFFF;
 
                  unsigned int layoutComponent        : 3;
     static const unsigned int layoutComponentEnd =     4;
