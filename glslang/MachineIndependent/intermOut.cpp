ed5fd5d8468b23ed6d0d12f09b14246e32feb68f
David Neto <dneto@google.com>
2016-02-28 23:32:44 -0500

    Support compilation via MinGW
    Change-Id: Ie52f0b1b2b20948c6f4b3cb5474537d36a5a3385

--- glslang/MachineIndependent/intermOut.cpp
+++ glslang/MachineIndependent/intermOut.cpp
@@ -48,7 +48,7 @@ namespace {
 bool is_positive_infinity(double x) {
 #ifdef _MSC_VER
   return _fpclass(x) == _FPCLASS_PINF;
-#elif defined __ANDROID__ || defined __linux__
+#elif defined __ANDROID__ || defined __linux__ || __MINGW32__ || __MINGW64__
   return std::isinf(x) && (x >= 0);
 #else
   return isinf(x) && (x >= 0);
