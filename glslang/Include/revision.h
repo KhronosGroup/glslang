fbb58a4e0b4e88c0849421a487edb85c660df13c
John Kessenich <cepheus@frii.com>
2017-02-03 11:21:24 -0700

    Build: Fix Linux for older compiler: Use TMap instead of TUndorderedMap.

--- glslang/Include/revision.h
+++ glslang/Include/revision.h
@@ -2,5 +2,5 @@
 // For the version, it uses the latest git tag followed by the number of commits.
 // For the date, it uses the current date (when then script is run).
 
-#define GLSLANG_REVISION "Overload400-PrecQual.1783"
-#define GLSLANG_DATE "01-Feb-2017"
+#define GLSLANG_REVISION "Overload400-PrecQual.1786"
+#define GLSLANG_DATE "03-Feb-2017"
