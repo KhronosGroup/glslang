db2e3b416930cca6c2c4dca2602b32d204741b6c
steve-lunarg <steve_gh@khasekhemwy.net>
2017-03-31 12:47:34 -0600

    HLSL: fix crash on empty sequence node passed to intrinsic expansions

--- gtests/Hlsl.FromFile.cpp
+++ gtests/Hlsl.FromFile.cpp
@@ -93,6 +93,7 @@ INSTANTIATE_TEST_CASE_P(
         {"hlsl.calculatelod.dx10.frag", "main"},
         {"hlsl.calculatelodunclamped.dx10.frag", "main"},
         {"hlsl.cast.frag", "PixelShaderFunction"},
+        {"hlsl.clip.frag", "main"},
         {"hlsl.comparison.vec.frag", "main"},
         {"hlsl.conditional.frag", "PixelShaderFunction"},
         {"hlsl.constructexpr.frag", "main"},
