55ba3eaf8977d9c917bd44d61d650f371a70536d
Christoph Kubisch <ckubisch@nvidia.com>
2019-04-13 22:18:16 +0200

    introduce new --dump-builtin-symbols command line
    add corresponding EShMsgBuiltinSymbolTable
    TSymbol::dump functions have option to do "complete" print
    bugfix in TType::getCompleteString, structure can be null for block

--- glslang/MachineIndependent/ShaderLang.cpp
+++ glslang/MachineIndependent/ShaderLang.cpp
@@ -377,6 +377,8 @@ bool InitializeSymbolTables(TInfoSink& infoSink, TSymbolTable** commonTable,  TS
                                    infoSink, commonTable, symbolTables);
 #endif
 
+
+
     return true;
 }
 
@@ -472,6 +474,14 @@ void SetupBuiltinSymbolTable(int version, EProfile profile, const SpvVersion& sp
     SetThreadPoolAllocator(&previousAllocator);
 
     glslang::ReleaseGlobalLock();
+}
+
+// Function to Print all builtins
+void DumpBuiltinSymbolTable(TInfoSink& infoSink, const TSymbolTable& symbolTable)
+{
+    infoSink.debug << "BuiltinSymbolTable {\n";
+    symbolTable.dump(infoSink, true);
+    infoSink.debug << "}\n";
 }
 
 // Return true if the shader was correctly specified for version/profile/stage.
@@ -903,6 +913,10 @@ bool ProcessDeferred(
     if (! AddContextSpecificSymbols(resources, compiler->infoSink, *symbolTable, version, profile, spvVersion,
                                     stage, source)) {
         return false;
+    }
+
+    if (messages & EShMsgBuiltinSymbolTable) {
+        DumpBuiltinSymbolTable(compiler->infoSink, *symbolTable);
     }
 
     //
