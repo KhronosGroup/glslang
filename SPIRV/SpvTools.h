f05c076e26b04cbcc9bf2df815bbdc9c620d89ad
John Kessenich <cepheus@frii.com>
2020-09-27 18:15:41 -0600

    Revert "Add more flexible SpirvToolsDisassemble interface to allow specifying spv_target_env for disassembly output. (#2406)"
    This reverts commit 2eed8236d07d50f35c466d723651d86284c60161.

--- SPIRV/SpvTools.h
+++ SPIRV/SpvTools.h
@@ -44,7 +44,6 @@
 #ifdef ENABLE_OPT
 #include <vector>
 #include <ostream>
-#include "spirv-tools/libspirv.h"
 #endif
 
 #include "glslang/MachineIndependent/localintermediate.h"
@@ -65,12 +64,8 @@ struct SpvOptions {
 
 #ifdef ENABLE_OPT
 
-// Use the SPIRV-Tools disassembler to print SPIR-V using a SPV_ENV_UNIVERSAL_1_3 environment.
+// Use the SPIRV-Tools disassembler to print SPIR-V.
 void SpirvToolsDisassemble(std::ostream& out, const std::vector<unsigned int>& spirv);
-
-// Use the SPIRV-Tools disassembler to print SPIR-V with a provided SPIR-V environment.
-void SpirvToolsDisassemble(std::ostream& out, const std::vector<unsigned int>& spirv,
-                           spv_target_env requested_context);
 
 // Apply the SPIRV-Tools validator to generated SPIR-V.
 void SpirvToolsValidate(const glslang::TIntermediate& intermediate, std::vector<unsigned int>& spirv,
