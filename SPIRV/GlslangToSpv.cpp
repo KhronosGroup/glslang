446a014252a8bd14f1874420cb9ae85c099cad3e
David Neto <dneto@google.com>
2020-11-12 13:24:16 -0500

    Avoid spuriously adding Geometry capability for vert, tesc, tese
    Use of gl_Layer and gl_ViewportIndex in tessellation and vertex
    shaders should not trigger the addition of the Geometry capability.
    
    Fixes #2461
    
    Added tests for use of gl_Layer and gl_ViewportIndex in a tessellation
    evaluation shader.
    
    Several tests for NVIDIA features for tessellation, vertex, or mesh
    shaders now lose the Geometry or MultiViewport capabilities.
    This is ok because the functionality is already covered by
    the ShaderViewportIndexLayerNV capability.
    
    The spv.meshShaderPerViewBuiltins.mesh test now fails validation
    because the validator does not know that PrimitiveId (and possibly
    other) builtins are enabled by the MeshShadingNV capability.
    I filed https://github.com/KhronosGroup/SPIRV-Headers/issues/179 to
    fix the grammar upstream.

--- SPIRV/GlslangToSpv.cpp
+++ SPIRV/GlslangToSpv.cpp
@@ -709,7 +709,10 @@ spv::BuiltIn TGlslangToSpvTraverser::TranslateBuiltInDecoration(glslang::TBuiltI
         return spv::BuiltInCullDistance;
 
     case glslang::EbvViewportIndex:
-        builder.addCapability(spv::CapabilityMultiViewport);
+        if (glslangIntermediate->getStage() == EShLangGeometry ||
+            glslangIntermediate->getStage() == EShLangFragment) {
+            builder.addCapability(spv::CapabilityMultiViewport);
+        }
         if (glslangIntermediate->getStage() == EShLangVertex ||
             glslangIntermediate->getStage() == EShLangTessControl ||
             glslangIntermediate->getStage() == EShLangTessEvaluation) {
@@ -738,7 +741,10 @@ spv::BuiltIn TGlslangToSpvTraverser::TranslateBuiltInDecoration(glslang::TBuiltI
         if (glslangIntermediate->getStage() == EShLangMeshNV) {
             return spv::BuiltInLayer;
         }
-        builder.addCapability(spv::CapabilityGeometry);
+        if (glslangIntermediate->getStage() == EShLangGeometry ||
+            glslangIntermediate->getStage() == EShLangFragment) {
+            builder.addCapability(spv::CapabilityGeometry);
+        }
         if (glslangIntermediate->getStage() == EShLangVertex ||
             glslangIntermediate->getStage() == EShLangTessControl ||
             glslangIntermediate->getStage() == EShLangTessEvaluation) {
