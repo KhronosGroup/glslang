914c09e1be74f42361fde1f32757dc1ad1b7a25b
Ralf Habacker <ralf.habacker@freenet.de>
2021-11-05 08:43:39 +0100

    Fix test-glslang tests

--- ctests/client-package/test-glslang.cpp
+++ ctests/client-package/test-glslang.cpp
@@ -1,8 +1,13 @@
 
-#include <glslang/Public/ShaderLang.h>
+#include <glslang/build_info.h>
+
+#include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    ShInitialize();
-    return 0;
+    char version[1024];
+    sprintf(version, "%d.%d.%d", GLSLANG_VERSION_MAJOR, GLSLANG_VERSION_MINOR, GLSLANG_VERSION_PATCH);
+
+    return strcmp(CHECK_VERSION, version) == 0 ? 0 : 1;
 }
