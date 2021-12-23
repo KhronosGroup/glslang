95e15366e7e675e89b4b4b6e776e73d129c2c271
Jeremy Hayes <jeremy@lunarg.com>
2021-12-23 14:53:44 -0700

    Unblock unsized error from assert
    Fix issue #2846.

--- gtests/AST.FromFile.cpp
+++ gtests/AST.FromFile.cpp
@@ -235,6 +235,7 @@ INSTANTIATE_TEST_SUITE_P(
         "findFunction.frag",
         "constantUnaryConversion.comp",
         "xfbUnsizedArray.error.vert",
+        "xfbUnsizedArray.error.tese",
         "glsl.140.layoutOffset.error.vert",
         "glsl.430.layoutOffset.error.vert",
         "glsl.450.subgroup.frag",
