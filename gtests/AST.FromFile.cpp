1b01aaaf2972de8552553351b9ccb1b23bfadc94
ZhiqianXia <xzq0528@outlook.com>
2022-01-04 10:40:04 +0800

    EndStreamPrimitive not supported when there is #extension GL_ARB_gpu_shader5
    Signed-off-by: ZhiqianXia <xzq0528@outlook.com>

--- gtests/AST.FromFile.cpp
+++ gtests/AST.FromFile.cpp
@@ -291,6 +291,7 @@ INSTANTIATE_TEST_SUITE_P(
         "GL_ARB_draw_instanced.vert",
         "GL_ARB_fragment_coord_conventions.vert",
         "BestMatchFunction.vert",
+        "EndStreamPrimitive.geom"
     })),
     FileNameAsCustomTestSuffix
 );
