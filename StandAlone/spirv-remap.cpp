829a00464bf93568cbd2001fc50e53aef34e113a
Allan MacKinnon <allanmac@alum.mit.edu>
2023-05-22 16:47:41 -0400

    `spirv-remap`: Support outputting each SPIR-V module to a filename
    This PR supports the current behavior and new behavior:
      * Outputting one or more inputs to a single directory (current
        behavior).
      * One output file per input (new behavior).
    
    If there is only a single output then the output is probed to
    determine if it's a directory using C++17
    `std::filesystem::is_directory()`.

--- StandAlone/spirv-remap.cpp
+++ StandAlone/spirv-remap.cpp
@@ -37,14 +37,7 @@
 #include <fstream>
 #include <cstring>
 #include <stdexcept>
-
-//
-// Enhanced output support depends on C++17 `std:filesystem::is_directory()`
-//
-#if (__cplusplus >= 201703L) && __has_include(<filesystem>)
 #include <filesystem>
-#define SPIRV_REMAP_CPP_LIB_FILESYSTEM
-#endif
 
 //
 // Include remapper
@@ -398,17 +391,6 @@ int main(int argc, char** argv)
 
     const bool isMultiInput      = inputFiles.size() > 1;
     const bool isMultiOutput     = outputDirOrFiles.size() > 1;
-
-    // Only support multiple outputs if C++17 and std::filesystem are
-    // available.
-#ifndef SPIRV_REMAP_CPP_LIB_FILESYSTEM
-    if (isMultiOutput)
-        {
-            usage(argv[0], "Multiple outputs are not supported.");
-        }
-
-    const bool isSingleOutputDir = true;
-#else
     const bool isSingleOutputDir = !isMultiOutput && std::filesystem::is_directory(outputDirOrFiles[0]);
 
     if (isMultiInput && !isMultiOutput && !isSingleOutputDir)
@@ -420,7 +402,6 @@ int main(int argc, char** argv)
         {
             usage(argv[0], "Output must be either a single directory or one output file per input.");
         }
-#endif
 
     // Main operations: read, remap, and write.
     execute(inputFiles, outputDirOrFiles, isSingleOutputDir, whiteListFile, opts, verbosity);
