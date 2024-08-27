9cd7ca26a20fa28ed5296e32e7070fd1a457f01f
U-NVIDIA.COM\rjennings <rjennings@nvidia.com>
2024-08-26 20:31:19 -0700

    PP: Don't report certain error about '#' when #if'd out
    Don't report the following error when scanning inactive code (e.g. code inside #if 0):
    "error: '#' : (#) can be preceded in its line only by spaces or horizontal tab"
    Adds a variable to PpContext to say whether we're currently skipping over an inactive #if/#ifdef/#elif/#else, and don't report the error inside scanToken if true.
    
    fixes 3704

--- gtests/Pp.FromFile.cpp
+++ gtests/Pp.FromFile.cpp
@@ -69,6 +69,7 @@ INSTANTIATE_TEST_SUITE_P(
         "preprocessor.eof_missing.vert",
         "preprocess.arb_shading_language_include.vert",
         "preprocess.include_directive_missing_extension.vert",
+        "preprocess.inactive_stringify.vert"
     })),
     FileNameAsCustomTestSuffix
 );
