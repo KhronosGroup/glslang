1375a70478d67f81612854c9258dd0055757fae9
conley <conleylee@foxmail.com>
2025-06-20 20:51:42 +0800

    add test protocol

--- glsld/test_protocol.cc
+++ glsld/test_protocol.cc
@@ -1,5 +1,36 @@
 #include "protocol.hpp"
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <unistd.h>
+#include <vector>
 
-int main(int argc, const char* argv[])
+int main(int argc, char* argv[])
 {
+    int ch = -1;
+    std::vector<std::string> files;
+    while ((ch = ::getopt(argc, argv, "f:")) != -1) {
+        switch (ch) {
+        case 'f':
+            files.push_back(optarg);
+            continue;
+        default:
+            break;
+        }
+    }
+
+    if (files.empty()) {
+        fprintf(stderr, "input 0 files\n");
+        return -1;
+    }
+
+    static Protocol protocol;
+
+    for (auto const& f : files) {
+        std::ifstream ifs(f);
+        nlohmann::json body = nlohmann::json::parse(ifs);
+		protocol.handle(body);
+    }
+
+    return 0;
 }
