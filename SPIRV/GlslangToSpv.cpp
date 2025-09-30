c42661b75fc096ebb218c6938e889b59cb3a37cf
Barnabás Pőcze <pobrn@protonmail.com>
2025-09-30 18:29:38 +0200

    spirv: only set LocalSizeId mode when necessary
    Only use `LocalSizeId` when necessary, same changes as #3351
    but applied to mesh and task shaders.
    
    Fixes #3739

--- SPIRV/GlslangToSpv.cpp
+++ SPIRV/GlslangToSpv.cpp
@@ -1984,7 +1984,7 @@ TGlslangToSpvTraverser::TGlslangToSpvTraverser(unsigned int spvVersion,
         break;
     }
     case EShLangTask:
-    case EShLangMesh:
+    case EShLangMesh: {
         if(isMeshShaderExt) {
             builder.addCapability(spv::Capability::MeshShadingEXT);
             builder.addExtension(spv::E_SPV_EXT_mesh_shader);
@@ -1992,7 +1992,14 @@ TGlslangToSpvTraverser::TGlslangToSpvTraverser(unsigned int spvVersion,
             builder.addCapability(spv::Capability::MeshShadingNV);
             builder.addExtension(spv::E_SPV_NV_mesh_shader);
         }
-        if (glslangIntermediate->getSpv().spv >= glslang::EShTargetSpv_1_6) {
+        bool needSizeId = false;
+        for (int dim = 0; dim < 3; ++dim) {
+            if ((glslangIntermediate->getLocalSizeSpecId(dim) != glslang::TQualifier::layoutNotSet)) {
+                needSizeId = true;
+                break;
+            }
+        }
+        if (glslangIntermediate->getSpv().spv >= glslang::EShTargetSpv_1_6 && needSizeId) {
             std::vector<spv::Id> dimConstId;
             for (int dim = 0; dim < 3; ++dim) {
                 bool specConst = (glslangIntermediate->getLocalSizeSpecId(dim) != glslang::TQualifier::layoutNotSet);
@@ -2024,7 +2031,7 @@ TGlslangToSpvTraverser::TGlslangToSpvTraverser(unsigned int spvVersion,
                 builder.addExecutionMode(shaderEntry, (spv::ExecutionMode)mode);
         }
         break;
-
+    }
     default:
         break;
     }
