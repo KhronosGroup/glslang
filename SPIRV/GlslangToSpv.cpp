1b3701203c5874d7e3769538ccc1de2d33d83ac5
Barnabás Pőcze <pobrn@protonmail.com>
2025-09-30 18:35:34 +0200

    spirv: remove unnecessary assignment
    Changing the value of `needSizeId` in the loop will have no
    effect because it is not checked after that anywhere.

--- SPIRV/GlslangToSpv.cpp
+++ SPIRV/GlslangToSpv.cpp
@@ -2057,7 +2057,6 @@ TGlslangToSpvTraverser::TGlslangToSpvTraverser(unsigned int spvVersion,
                 if (specConst) {
                     builder.addDecoration(dimConstId.back(), spv::Decoration::SpecId,
                                           glslangIntermediate->getLocalSizeSpecId(dim));
-                    needSizeId = true;
                 }
             }
             builder.addExecutionModeId(shaderEntry, spv::ExecutionMode::LocalSizeId, dimConstId);
