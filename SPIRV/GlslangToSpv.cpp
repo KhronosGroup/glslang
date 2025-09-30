1cf6297e19482165dbb963db1788fba222cc537e
Barnabás Pőcze <pobrn@protonmail.com>
2025-09-30 18:20:52 +0200

    spirv: use `LocalSizeId` exec mode when targeting >= SPIR-V 1.2
    `OpExecutionModeId` and the `LocalSizeId` execution mode were added in
    SPIR-V 1.2[0]. So use them when targeting at least 1.2.
    
    [0]: https://registry.khronos.org/SPIR-V/specs/unified1/SPIRV.html#OpExecutionModeId
    
    See #3510

--- SPIRV/GlslangToSpv.cpp
+++ SPIRV/GlslangToSpv.cpp
@@ -2026,7 +2026,7 @@ TGlslangToSpvTraverser::TGlslangToSpvTraverser(unsigned int spvVersion,
                 break;
             }
         }
-        if (glslangIntermediate->getSpv().spv >= glslang::EShTargetSpv_1_6 && needSizeId) {
+        if (glslangIntermediate->getSpv().spv >= glslang::EShTargetSpv_1_2 && needSizeId) {
             std::vector<spv::Id> dimConstId;
             for (int dim = 0; dim < 3; ++dim) {
                 bool specConst = (glslangIntermediate->getLocalSizeSpecId(dim) != glslang::TQualifier::layoutNotSet);
@@ -2177,7 +2177,7 @@ TGlslangToSpvTraverser::TGlslangToSpvTraverser(unsigned int spvVersion,
                 break;
             }
         }
-        if (glslangIntermediate->getSpv().spv >= glslang::EShTargetSpv_1_6 && needSizeId) {
+        if (glslangIntermediate->getSpv().spv >= glslang::EShTargetSpv_1_2 && needSizeId) {
             std::vector<spv::Id> dimConstId;
             for (int dim = 0; dim < 3; ++dim) {
                 bool specConst = (glslangIntermediate->getLocalSizeSpecId(dim) != glslang::TQualifier::layoutNotSet);
